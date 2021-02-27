3203878a3db7418639cb4219fd8a0c616ffb699f
akinsanya <michael_aka1@icloud.com>
2021-02-27 20:58:15 +0100

    Loop Statements practice

---
+++ Check.java
@@ -0,0 +1,8 @@
+public class Check{
+    public static void main(String... args){
+
+            for(int takecharge = 10; takecharge >= 1 ;takecharge--){
+                System.out.println(takecharge);
+            }
+      }
+}
